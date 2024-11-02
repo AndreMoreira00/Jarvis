13b0b09d1c44c8fa572e42f7f71a0ba00a18c16b
AndreMoreira00 <andremoreira102030@gmail.com>
2024-11-02 20:59:05 -0300

    Variaveis dos pontos

--- Vision_AI.py
+++ Vision_AI.py
@@ -18,8 +18,7 @@ mp_drawing = mp.solutions.drawing_utils
 def save_foto(frame):
     timesr = time.strftime("%Y%m%d_%H%M%S")
     cv2.imwrite(f"Images/{timesr}.jpg", frame)
-    print(f"Foto tirada e salva como '{timesr}.jpg'")
-    time.sleep(0.5)
+    time.sleep(0.3)
 
 # Gravar Video:
 
@@ -65,76 +64,80 @@ while cap.isOpened():
 
     if results.multi_hand_landmarks and results.multi_handedness:
         for hand_landmarks, hand_handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-            hand_label = hand_handedness.classification[0].label  # "Right" ou "Left"
+            
+            # "Right" ou "Left":
+            hand_label = hand_handedness.classification[0].label  
+            
+            # Definindo variaves dos pontos da mão
             
             # Palmo:
-            # palmo = hand_landmarks.landmark[0]
+            palmo = hand_landmarks.landmark[0]
             
             # Polegar:
             polegar_4 = hand_landmarks.landmark[4]
-            # polegar_3 = hand_landmarks.landmark[3]
-            # polegar_2 = hand_landmarks.landmark[2]
-            # polegar_1 = hand_landmarks.landmark[1]
+            polegar_3 = hand_landmarks.landmark[3]
+            polegar_2 = hand_landmarks.landmark[2]
+            polegar_1 = hand_landmarks.landmark[1]
             
             # Indicador:
             indicador_8 = hand_landmarks.landmark[8]
-            # indicador_7 = hand_landmarks.landmark[7]
-            # indicador_6 = hand_landmarks.landmark[6]
-            # indicador_5 = hand_landmarks.landmark[5]
+            indicador_7 = hand_landmarks.landmark[7]
+            indicador_6 = hand_landmarks.landmark[6]
+            indicador_5 = hand_landmarks.landmark[5]
             
             # Meio:
             meio_12 = hand_landmarks.landmark[12]
-            # meio_11 = hand_landmarks.landmark[11]
-            # meio_10 = hand_landmarks.landmark[10]
-            # meio_9 = hand_landmarks.landmark[9]
+            meio_11 = hand_landmarks.landmark[11]
+            meio_10 = hand_landmarks.landmark[10]
+            meio_9 = hand_landmarks.landmark[9]
 
             # Anelar:
             anelar_16 = hand_landmarks.landmark[16]
-            # anelar_15 = hand_landmarks.landmark[15]
-            # anelar_14 = hand_landmarks.landmark[14]
-            # anelar_13 = hand_landmarks.landmark[13]
+            anelar_15 = hand_landmarks.landmark[15]
+            anelar_14 = hand_landmarks.landmark[14]
+            anelar_13 = hand_landmarks.landmark[13]
             
             # Mindinho:
             mindinho_20 = hand_landmarks.landmark[20]
-            # mindinho_19 = hand_landmarks.landmark[19]
-            # mindinho_18 = hand_landmarks.landmark[18]
-            # mindinho_17 = hand_landmarks.landmark[17]
+            mindinho_19 = hand_landmarks.landmark[19]
+            mindinho_18 = hand_landmarks.landmark[18]
+            mindinho_17 = hand_landmarks.landmark[17]
 
             # Converte para pixels
             h, w, _ = frame.shape
             
             # Palmo:
-            # palmo_x, palmo_y = int(palmo.x * w), int(palmo.y * h)
+            palmo_x, palmo_y = int(palmo.x * w), int(palmo.y * h)
             
             # Polegar:
             polegar_4_x, polegar_4_y = int(polegar_4.x * w), int(polegar_4.y * h)
-            # polegar_3_x, polegar_3_y = int(polegar_3.x * w), int(polegar_3.y * h)
-            # polegar_2_x, polegar_2_y = int(polegar_2.x * w), int(polegar_2.y * h)
-            # polegar_1_x, polegar_1_y = int(polegar_1.x * w), int(polegar_1.y * h)
+            polegar_3_x, polegar_3_y = int(polegar_3.x * w), int(polegar_3.y * h)
+            polegar_2_x, polegar_2_y = int(polegar_2.x * w), int(polegar_2.y * h)
+            polegar_1_x, polegar_1_y = int(polegar_1.x * w), int(polegar_1.y * h)
             
             # Indicador:
             indicador_8_x, indicador_8_y = int(indicador_8.x * w), int(indicador_8.y * h)
-            # indicador_7_x, indicador_7_y = int(indicador_7.x * w), int(indicador_7.y * h)
-            # indicador_6_x, indicador_6_y = int(indicador_6.x * w), int(indicador_6.y * h)
-            # indicador_5_x, indicador_5_y = int(indicador_5.x * w), int(indicador_5.y * h)
+            indicador_7_x, indicador_7_y = int(indicador_7.x * w), int(indicador_7.y * h)
+            indicador_6_x, indicador_6_y = int(indicador_6.x * w), int(indicador_6.y * h)
+            indicador_5_x, indicador_5_y = int(indicador_5.x * w), int(indicador_5.y * h)
             
             # Meio:
             meio_12_x, meio_12_y = int(meio_12.x * w), int(meio_12.y * w)
-            # meio_11_x, meio_11_y = int(meio_11.x * w), int(meio_11.y * w)
-            # meio_10_x, meio_10_y = int(meio_10.x * w), int(meio_10.y * w)
-            # meio_9_x, meio_9_y = int(meio_9.x * w), int(meio_9.y * w)
+            meio_11_x, meio_11_y = int(meio_11.x * w), int(meio_11.y * w)
+            meio_10_x, meio_10_y = int(meio_10.x * w), int(meio_10.y * w)
+            meio_9_x, meio_9_y = int(meio_9.x * w), int(meio_9.y * w)
             
             # Anelar:
             anelar_16_x, anelar_16_y = int(anelar_16.x * w), int(anelar_16.y * w)
-            # anelar_15_x, anelar_15_y = int(anelar_15.x * w), int(anelar_15.y * w)
-            # anelar_14_x, anelar_14_y = int(anelar_14.x * w), int(anelar_14.y * w)
-            # anelar_13_x, anelar_13_y = int(anelar_13.x * w), int(anelar_13.y * w)
+            anelar_15_x, anelar_15_y = int(anelar_15.x * w), int(anelar_15.y * w)
+            anelar_14_x, anelar_14_y = int(anelar_14.x * w), int(anelar_14.y * w)
+            anelar_13_x, anelar_13_y = int(anelar_13.x * w), int(anelar_13.y * w)
             
             # Mindinho:
             mindinho_20_x, mindinho_20_y = int(mindinho_20.x * w), int(mindinho_20.y * w)
-            # mindinho_19_x, mindinho_19_y = int(mindinho_19.x * w), int(mindinho_19.y * w)
-            # mindinho_18_x, mindinho_18_y = int(mindinho_18.x * w), int(mindinho_18.y * w)
-            # mindinho_17_x, mindinho_17_y = int(mindinho_17.x * w), int(mindinho_17.y * w)
+            mindinho_19_x, mindinho_19_y = int(mindinho_19.x * w), int(mindinho_19.y * w)
+            mindinho_18_x, mindinho_18_y = int(mindinho_18.x * w), int(mindinho_18.y * w)
+            mindinho_17_x, mindinho_17_y = int(mindinho_17.x * w), int(mindinho_17.y * w)
             
             # Verificação de Gestos: 
           
