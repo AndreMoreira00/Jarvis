0f947d4507425a9fb3a49cc7f73da9bf7f10b2d3
AndreMoreira00 <andremoreira102030@gmail.com>
2025-01-13 14:38:35 -0300

    Nova organizacao esta sendo contruida so falta a parte de Hands e do main

---
+++ New organized template/control.py
@@ -0,0 +1,90 @@
+import time
+import cv2
+import pyaudio
+import wave
+import speech_recognition as sr
+from concurrent.futures import ThreadPoolExecutor
+
+import jarvis
+
+jarvis_system = jarvis.Jarvis()
+
+class Control:
+  def __init__(self):
+    pass
+  
+  # Capture Photo
+  def Capture_Photo(self, frame):
+    timesr = time.strftime("%Y%m%d_%H%M%S")
+    cv2.imwrite(f"./{timesr}.jpg", frame)
+    time.sleep(0.5)
+    return f"./{timesr}.jpg"
+  
+  # Capture Video
+  def Capture_Video(self, cap):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    timesr = time.strftime("%Y%m%d_%H%M%S")
+    duration_in_seconds = 15
+    fps = 30
+    out = cv2.VideoWriter(f'./{timesr}.avi', fourcc, fps, (640, 480))
+    total_frames = duration_in_seconds * fps
+    frame_count = 0
+    while frame_count < total_frames:
+        status, frame = cap.read()
+        out.write(frame)
+        frame_count+=1
+    return f'./{timesr}.avi'
+  
+  # Capture Audio
+  def Capture_Audio(self):
+    audio = pyaudio.PyAudio()
+    stream = audio.open(
+      input = True,
+      format = pyaudio.paInt16,
+      channels = 1,
+      rate = 44000,
+      frames_per_buffer = 1024,
+    )
+    frames = []
+    timeout = 15
+    timeout_start = time.time()
+    try:
+      while time.time() < timeout_start + timeout:
+        bloco = stream.read(1024)
+        frames.append(bloco)
+    except KeyboardInterrupt:
+        pass
+    arquivo_final = wave.open("./gravacao.wav", "wb")
+    arquivo_final.setnchannels(1)
+    arquivo_final.setframerate(44000)
+    arquivo_final.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    arquivo_final.writeframes(b"".join(frames))
+    arquivo_final.close()
+    r = sr.Recognizer()
+    with sr.WavFile("./gravacao.wav") as source:              
+      audio = r.record(source)                        
+    try:
+        return (""+r.recognize_google(audio, language="pt-BR"))
+    except LookupError:         
+        return("Sem perguntas!")
+
+  # Functions control
+  
+  ## Image Audio
+  async def Image_Audio(self, frame) -> None:
+    with ThreadPoolExecutor() as executor:
+      future_foto = executor.submit(self.Capture_Photo, frame)
+      future_audio = executor.submit(self.Capture_Audio)
+      image_path = future_foto.result()
+      prompt = future_audio.result()                       
+    await jarvis_system.Image_To_Text(image_path,prompt)
+    
+  ## Video Audio
+  async def Video_Audio(self, cap) -> None: 
+    with ThreadPoolExecutor() as executor:
+      future_video = executor.submit(self.Capture_Video, cap)
+      future_audio = executor.submit(self.Capture_Audio)
+      video_path = future_video.result()
+      prompt = future_audio.result()
+    
+      await jarvis_system.Video_To_Text(video_path, prompt)
---
+++ New organized template/jarvis.py
@@ -0,0 +1,69 @@
+import os
+import asyncio
+from dotenv import load_dotenv, find_dotenv
+import google.generativeai as genai
+import google
+import pathlib
+import speech_recognition as sr
+import edge_tts
+from playsound import playsound
+import time
+
+class Jarvis:
+    def __init__(self):
+      # Load Key
+      load_dotenv()
+      self.API_KEY = os.getenv("API_GEMINI")
+      # Template
+      self.template = """
+      Você é um assistente e eu sou o seu mestre. Você é designado a me ajudar para solucionar qualquer questão lógica que eu fizer. Sempre me trate e se refira a mim como "Mestre". Não use na sua resposta caracteres especiais como (***): 
+      """
+      # Config model Genai
+      genai.configure(api_key=self.API_KEY)
+      self.model = genai.GenerativeModel(
+          "gemini-1.5-flash", system_instruction=self.template
+      )
+      # Config Voice
+      VOICES = ["pt-BR-AntonioNeural"]
+      self.VOICE = VOICES[0]
+      self.OUTPUT_FILE = "./translate.mp3"
+      # Config Paths
+      self.PATH_FILE = "C:/Users/andre/OneDrive/Documentos/Jarvis/New organized template/translate.mp3"
+    
+    # Delete Cahche Video
+    def Delete_Cahche_Files(self):
+      for f in genai.list_files():
+          myfile = genai.get_file(f.name)
+          myfile.delete()
+          
+    # Translate voice from Jarvis
+    async def Translate(self, text) -> None:
+      communicate = edge_tts.Communicate(text, self.VOICE)
+      await communicate.save(self.OUTPUT_FILE)
+    
+    # Response Text to Text
+    async def Text_To_Text(self, prompt) -> None:
+      response = self.model.generate_content(prompt)
+      await self.Translate(response.text)
+      playsound(self.PATH_FILE)
+      
+    # Response Image to Text
+    async def Image_To_Text(self, image_path, prompt) -> None:
+      response = self.model.generate_content([{'mime_type':'image/jpeg', 'data': pathlib.Path(f'{image_path}').read_bytes()}, prompt])
+      await self.Translate(response.text)
+      playsound(self.PATH_FILE)
+    
+    # Response Video to text 
+    async def Video_To_Text(self, video_path, prompt) -> None:
+      video_file = genai.upload_file(path=video_path)
+      while video_file.state.name == "PROCESSING":
+        print('.', end='')
+        time.sleep(10)
+        video_file = genai.get_file(video_file.name)
+      if video_file.state.name == "FAILED":
+        raise ValueError(video_file.state.name)
+      response = self.model.generate_content([video_file, prompt],
+                                        request_options={"timeout": 600})
+      await self.Translate(response.text)
+      playsound(self.PATH_FILE)
+      self.Delete_Cahche_Files()
