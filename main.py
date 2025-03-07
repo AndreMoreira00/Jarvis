44042621a2102d052cf717b5ab20ecd270f6b0ad
AndreMoreira00 <andremoreira102030@gmail.com>
2025-03-07 16:08:02 -0300

    Verificacoes de gestos com thread 2

--- main.py
+++ main.py
@@ -24,58 +24,55 @@ async def main(): # Função de execução principal
   
   hands_system, control_functions = await asyncio.gather(hands_task, control_task) # Criação do objeto Hands e Control 
   
-  verificacoes = [
-    (control_functions.Capture_Photo(frame), hands_system.Map_Ok, "Right"),
-    (control_functions.Capture_Video(cap), hands_system.Map_Positive, "Left"),
-    (control_functions.Audio_to_Audio(), hands_system.Map_Speak, "Right"),
-    (control_functions.Image_Audio(frame), hands_system.Map_Squid, "Left"),
-    (control_functions.Video_Audio(cap), hands_system.Map_Rock, "Right"),
-  ]
-  
   # Preferencia de camera
-  cap = cv2.VideoCapture(1)
+  cap = cv2.VideoCapture(0)
+  
+  with ThreadPoolExecutor() as executor: # Torna as funções sincronas
+    
+    verificacoes = [
+      (lambda: control_functions.Capture_Photo(frame), hands_system.Map_Ok, "Right"),
+      (lambda: control_functions.Capture_Video(cap), hands_system.Map_Positive, "Left"),
+      (lambda: control_functions.Audio_to_Audio(), hands_system.Map_Speak, "Right"),
+      (lambda: control_functions.Image_Audio(frame), hands_system.Map_Squid, "Left"),
+      (lambda: control_functions.Video_Audio(cap), hands_system.Map_Rock, "Right"),
+    ]
+    
+    while cap.isOpened(): # Execulta as funçõoes de dentro enquanto a camera está aberta
+        ret, frame = cap.read() # Captura de cada frame da camera. Ret é um parametro para verificar a captura
 
-    # Execulta as funçõoes de dentro enquanto a camera está aberta
-  while cap.isOpened():
-      ret, frame = cap.read() # Captura de cada frame da camera. Ret é um parametro para verificar a captura
+        if not ret:
+            print("Erro ao capturar o frame.")
+            break
 
-      if not ret:
-          print("Erro ao capturar o frame.")
-          break
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Configuração de cores para a identificação das mãos
+        results = hands_system.hands.process(rgb_frame) # Resposta da identificação das mãos
+        
+        if results.multi_hand_landmarks and results.multi_handedness: # Marcaçãos dos pontos e retas nas mãos
 
-      rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Configuração de cores para a identificação das mãos
-      results = hands_system.hands.process(rgb_frame) # Resposta da identificação das mãos
-      
-      if results.multi_hand_landmarks and results.multi_handedness: # Marcaçãos dos pontos e retas nas mãos
-
-          for hand_landmarks, hand_handedness in zip(results.multi_hand_landmarks, results.multi_handedness): # Obtemos as previsões em tempo real dos pontos e das retas
-              
-              hand_label = hand_handedness.classification[0].label # Identificação da mão direita e esquerda
-              
-              h, w, _ = frame.shape # Constantes de proporção da camera h = heigth, w = width, _ = canais
-              
-              with ThreadPoolExecutor() as executor: # Torna as funções sincronas
+            for hand_landmarks, hand_handedness in zip(results.multi_hand_landmarks, results.multi_handedness): # Obtemos as previsões em tempo real dos pontos e das retas
+                
+                hand_label = hand_handedness.classification[0].label # Identificação da mão direita e esquerda
+                
+                h, w, _ = frame.shape # Constantes de proporção da camera h = heigth, w = width, _ = canais
                 
                 futures = {
-                  executor.submit(verificar_gesto, nome, func, h, w, hand_landmarks, frame): nome
-                  for nome, func, lado in verificacoes
+                  executor.submit(verificar_gesto, act, func, h, w, hand_landmarks, frame): act
+                  for act, func, lado in verificacoes
                   if hand_label == lado
                 }
                 
                 for future in futures:
-                  resultado = future.result()
-                  if resultado and not control_functions.ACTION:
-                      resultado
-                    
-              hands_system.mp_drawing.draw_landmarks(frame, hand_landmarks, hands_system.mp_hands.HAND_CONNECTIONS) # Reenderizar os pontos e retas na tela
-          
-      cv2.imshow("MediaPipe Hands", frame) # Criar uam tela com a visao da camera
-      
-      if cv2.waitKey(1) & 0xFF == ord('q'): # Encerra o programa clicando Q
-        break
-      
-  cap.release() # Fecha a camera
-  cv2.destroyAllWindows() # Destroi a tela da camera
+                  future.result()
+                      
+                hands_system.mp_drawing.draw_landmarks(frame, hand_landmarks, hands_system.mp_hands.HAND_CONNECTIONS) # Reenderizar os pontos e retas na tela
+            
+        cv2.imshow("MediaPipe Hands", frame) # Criar uam tela com a visao da camera
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'): # Encerra o programa clicando Q
+          break
+        
+    cap.release() # Fecha a camera
+    cv2.destroyAllWindows() # Destroi a tela da camera
 
 if __name__ == "__main__": # Verificação de arquivo principal com prioridade de execução
   asyncio.run(main()) # Execultar a função principal de forma assincrona
