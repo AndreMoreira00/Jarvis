734312cd3d32081372e597498e2e101131cf9ba4
AndreMoreira00 <andremoreira102030@gmail.com>
2025-04-22 18:21:01 -0300

    Ajuste na funcao refresh e nas bibliotecas defasadas na api da google fotos

--- manager.py
+++ manager.py
@@ -1,82 +1,86 @@
-import httplib2
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import run_flow
-import requests
 import os
-from concurrent.futures import ThreadPoolExecutor # Torna as funções sincronas
+import json
+import requests
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from concurrent.futures import ThreadPoolExecutor
 
 class Manager:
-  def __init__(self):
-    self.CLIENT_SECRET = './env/client_secret.json'
-    self.SCOPE = 'https://www.googleapis.com/auth/photoslibrary'
-    self.STORAGE = Storage('./env/credentials.storage') #Transforma em Json para credenciais 
-  
-  def authorize_credentials(self):
-    if os.path.exists('./env/credentials.storage'):
-      credentials = self.STORAGE.get() #Pega o conteudo do Json atual
-      if credentials and not credentials.invalid: #Verifica se o token é valido
-        if credentials.access_token_expired: # Verifica se o token experiou
-          credentials.refresh(httplib2.Http()) #Recria o token
-    else:
-      flow = flow_from_clientsecrets(self.CLIENT_SECRET, scope=self.SCOPE) # Caso não tenha o arquivo ele vai solicitar o acesso ao Host
-      http = httplib2.Http() # Requisicao HTTP
-      credentials = run_flow(flow, self.STORAGE, http=http) # Obtem uma credencial de um novo usario
-      print("Contact the HOST to access the Beta Test version.\nandremoreira102030@gmail.com\nSubject: Credentials Jarvir for client test.\nHello André Moreira, I wanted to test your Jarvis project, but I don't have credentials for Google Project.\nEmail: <your-email>. ") # Passa o convite de teste para novos usuarios
+    def __init__(self):
+        self.CLIENT_SECRET = './env/client_secret.json'
+        self.CREDENTIALS_FILE = './env/token.json'
+        self.SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
     
-    return credentials.access_token # Obtem o token da credencial
-  
-  def getPhotoUrl(self,access_token, photo_id):
-    headers = {
-        "Authorization": "Bearer " + access_token,
-        "Content-type": "application/json"
-    }
-    url = "https://photoslibrary.googleapis.com/v1/mediaItems/" + photo_id
-    response = requests.get(url, headers=headers)
-    response_json = response.json()
-    photo_url = response_json["baseUrl"]
-    return photo_url
-    
-  def uploadMidia(self, midia_path):
-    access_token = self.authorize_credentials()
-    
-    headers = {
-        'Authorization': 'Bearer %s' % access_token,
-        'Content-type': 'application/octet-stream',
-        'X-Goog-Upload-Content-Type': 'image/jpeg',
-        'X-Goog-Upload-Protocol': 'raw'
-    }
+    def authorize_credentials(self):
+        creds = None
 
-    with open(midia_path, 'rb') as f:
-        midia_data = f.read()
+        if os.path.exists(self.CREDENTIALS_FILE):
+            creds = Credentials.from_authorized_user_file(self.CREDENTIALS_FILE, self.SCOPES)
 
-    response = requests.post('https://photoslibrary.googleapis.com/v1/uploads',
-                            headers=headers, data=midia_data)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(self.CLIENT_SECRET, self.SCOPES)
+                creds = flow.run_local_server(port=0)
 
-    if response.status_code == requests.codes.ok:
-        upload_token = response.text
+            with open(self.CREDENTIALS_FILE, 'w') as token:
+                token.write(creds.to_json())
+
+        return creds.token 
+
+    def getPhotoUrl(self, access_token, photo_id):
         headers = {
-            'Authorization': 'Bearer %s' % access_token,
-            'Content-type': 'application/json'
+            "Authorization": f"Bearer {access_token}",
+            "Content-type": "application/json"
         }
-        payload = {
-            "newMediaItems": [
+        url = f"https://photoslibrary.googleapis.com/v1/mediaItems/{photo_id}"
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+        return response_json["baseUrl"]
+
+    def uploadMidia(self, image_path):
+        access_token = self.authorize_credentials()
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-type': 'application/octet-stream',
+            'X-Goog-Upload-Content-Type': 'image/jpeg',
+            'X-Goog-Upload-Protocol': 'raw'
+        }
+
+        with open(image_path, 'rb') as f:
+            image_data = f.read()
+
+        response = requests.post('https://photoslibrary.googleapis.com/v1/uploads',
+                                 headers=headers, data=image_data)
+
+        if response.status_code == 200:
+            upload_token = response.text
+            headers = {
+                'Authorization': f'Bearer {access_token}',
+                'Content-type': 'application/json'
+            }
+            payload = {
+                "newMediaItems": [
                     {
                         "simpleMediaItem": {
-                            "fileName": os.path.basename(midia_path),
+                            "fileName": os.path.basename(image_path),
                             "uploadToken": upload_token
                         }
                     }
                 ]
             }
-        
-        response = requests.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate',
-                                headers=headers, json=payload)   
-        json_response = response.json()
-        photo_id = json_response['newMediaItemResults'][0]['mediaItem']['id']
-        photo_url = self.getPhotoUrl(access_token, photo_id)
-    else:
-        response.raise_for_status()
+
+            response = requests.post(
+                'https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate',
+                headers=headers, json=payload
+            )
+            response_json = response.json()
+            photo_id = response_json['newMediaItemResults'][0]['mediaItem']['id']
+            photo_url = self.getPhotoUrl(access_token, photo_id)
+        else:
+            response.raise_for_status()
 
 # with ThreadPoolExecutor() as executor:
 #   maneger = Manager()
